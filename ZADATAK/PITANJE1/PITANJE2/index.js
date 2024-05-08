c0fe9ffd0ba67649227163a30bb0bfe67bce0065
Marijo12 <122817616+Marijo12@users.noreply.github.com>
2024-05-08 23:08:43 +0200

    Add files via upload

---
+++ ZADATAK/PITANJE1/PITANJE2/index.js
@@ -0,0 +1,13 @@
+alert("Congratulation, you found a hiddeng message...");
+alert("Now is something specific...");
+alert("You have to use math(mm, mm^2).")
+alert("Find two numbers and do something...");
+
+function potvrdi(){
+    var unos = document.getElementById("unos").value;
+    if(unos == 28258 ){
+        alert("Congrats");
+    }else{
+        alert("we are so sorry");
+    }
+}
---
+++ ZADATAK/PITANJE1/index.js
@@ -0,0 +1,11 @@
+alert("This is something like Cicada 3301, if you don't know what is that, search it...");
+alert("Use your sight, you have to check every anglee...");
+alert("Make sure that your zoom of browser is at 100% to the rest of the game.");
+function potvrdi(){
+    var unos = document.getElementById("unos").value;
+    if (unos == "Vedran"){
+        window.location.href = 'PITANJE2/index.html';
+    }else{
+        alert("We are so sorry...");
+    }
+}
